6b92b5503e5d930711f3317d324fb385099577ee
KBarr <ksamuels6@yahoo.com>
2020-08-19 17:25:53 -0500

    exercise 2 part 2

---
+++ src/exercises/aug19ArrayListPractice.java
@@ -0,0 +1,19 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class aug19ArrayListPractice {
+    public static void main(String[] args) {
+        int arrayList[] = {1, 1, 2, 3, 5, 8, 10, 12, 4, 33};
+        int evenSum = 0;
+        for (int i=0; i< arrayList.length; i++){
+            if (arrayList[i] % 2 == 0) {
+                evenSum = evenSum + arrayList[i];
+                System.out.println(evenSum);
+            }
+        }
+        String wordArray[] = {"Hello", "Goodbye", "lucky", "cat", "Troll"};
+        for(String i : wordArray.split[i])
+    }
+}
