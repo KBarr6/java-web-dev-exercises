d09c2c619b695b24d4903d65250925327733ac94
KBarr <ksamuels6@yahoo.com>
2020-08-24 21:40:40 -0500

    studio complete

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,8 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student keisha = new Student("Keisha Barr", 6780, 1, 4.0);
+
+        System.out.println("Keisha's gpa: " + keisha.getGpa());
     }
 }
